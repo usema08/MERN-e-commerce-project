e2536c2ba64b6d8684b5306069ab0020c2d230df
usema08 <112565978+usema08@users.noreply.github.com>
2024-01-23 22:41:42 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config();
 const port = process.env.PORT || 5000 ;
@@ -16,8 +17,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/",( req,res) => {
-    res.send("Hello World");
-})
+app.use('/api/users', userRoutes)
 
 app.listen( port, () => console.log(`Server running on port: ${port}`));
