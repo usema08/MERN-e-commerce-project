5f4327ca48608b22e38e26c19c673d6cf75b8983
Usema Amber <usemaamber85@gmail.com>
2024-01-24 23:12:14 +0530

    bcryptjs and JWT

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
     const {username, email, password} = req.body
@@ -11,11 +13,21 @@ const createUser = asyncHandler(async (req, res) => {
     const userExists = await User.findOne({email})
     if (userExists) res.status(400).send("User already exists.")
 
-    const newUser = new User({username, email, password})
+    const  salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({username, email, password: hashedPassword})
 
     try {
         await newUser.save()
-        res.status(201).json({_id: newUser._id, username: newUser.username, email:newUser.email, isAdmin: newUser.isAdmin})
+        createToken(res, newUser._id);
+
+        res.status(201).json({
+            _id: newUser._id, 
+            username: newUser.username, 
+            email:newUser.email, 
+            isAdmin: newUser.isAdmin,
+        })
 
     } catch (error) {
         res.status(400)
